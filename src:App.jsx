17cddc7321a1e8a793393574d3384d8c8c41fa60
Brigita Stankeviciute <152437949+BriStanke@users.noreply.github.com>
2026-03-28 01:30:33 +0000

    Update src:App.jsx

--- src:App.jsx
+++ src:App.jsx
@@ -1,10 +1,3 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
-
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
